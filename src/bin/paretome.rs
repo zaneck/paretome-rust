f5888341d68c2c4e9ede1ed31145c507b4001e30
Zaneck <val.owczarek@gmail.com>
2025-01-25 14:50:57 +0100

    up

--- src/bin/paretome.rs
+++ src/bin/paretome.rs
@@ -1,3 +1,6 @@
+use same_file::Handle;
+use std::path::Path;
+
 use paretome_rust::pareto::add;
 use structopt::StructOpt;
 
@@ -5,13 +8,30 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Cli {
     /// The raw data to pareto sort.
-    input: String,
+    input: Option<String>,
     /// The pareto front.
-    output: String,
+    output: Option<String>,
 }
 
 fn main(){
     let args = Cli::from_args();
     println!("Hello, Paretome! {}", add(2, 2));
-    println!("input and output files: {}, {}", args.input, args.output);
+
+    let input: Result<Handle, std::io::Error> = if args.input.is_none(){
+        same_file::Handle::stdin()
+    }
+    else {
+        let input_path = Path::new(args.input.as_ref().unwrap().as_str());
+        same_file::Handle::from_path(input_path)
+    };
+
+    let output: Result<Handle, std::io::Error> = if args.output.is_none(){
+        same_file::Handle::stdout()
+    }
+    else {
+        let output_path = Path::new(args.input.as_ref().unwrap().as_str());
+        same_file::Handle::from_path(output_path)
+    };
+
+    println!("input and output files: {:?}, {:?}", input, output);
 }
